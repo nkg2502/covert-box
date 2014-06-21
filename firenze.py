7aef0bcf9a2323dff60a5a5a779bcd5c033b535a
Taesung Jung <tsj.july@gmail.com>
2014-06-21 15:26:52 +0900

    implemented symmetric key feature closed #1 added symmetric key page (/download) closed #2

--- firenze.py
+++ firenze.py
@@ -4,6 +4,8 @@
 import os
 import urllib
 
+from google.appengine.ext import ndb
+
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 
@@ -14,6 +16,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 		loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
 		extensions = ['jinja2.ext.autoescape'],
 		autoescape = True)
+
+class CovertBox(ndb.Model):
+	key = ndb.BlobKeyProperty()
+	email = ndb.StringProperty()
+	generated_key = ndb.StringProperty()
+	file_name = ndb.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
@@ -26,6 +34,15 @@ class MainHandler(webapp2.RequestHandler):
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
 	def post(self):
 		upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+		user_key = self.request.get('user_key')
+
+		box_instance = CovertBox(parent=ndb.Key('user_key', user_key))
+		box_instance.key = upload_files[0].key()
+		box_instance.email = 'n'
+		box_instance.generated_key = 'n'
+		box_instance.file_name = upload_files[0].filename
+		box_instance.put()
+
 		self.redirect('/done')
 
 class DoneHandler(webapp2.RequestHandler):
@@ -33,7 +50,36 @@ class DoneHandler(webapp2.RequestHandler):
 		done_page = JINJA_ENVIRONMENT.get_template('done.html')
 		self.response.out.write(done_page.render({}))
 
+class DownloadHandler(webapp2.RequestHandler):
+	def get(self):
+		page = JINJA_ENVIRONMENT.get_template('gate.html')
+		self.response.out.write(page.render({}))
+
+	def post(self):
+		page = JINJA_ENVIRONMENT.get_template('download.html')
+
+		user_key = self.request.get('user_key')
+
+		box_query = CovertBox.query(ancestor=ndb.Key('user_key', user_key))
+		box_list = box_query.fetch()
+
+		page_value = {
+				'list': box_list 
+		}
+
+		self.response.out.write(page.render(page_value))
+
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+	def get(self, resource):
+		resource = str(urllib.unquote(resource))
+		blob_info = blobstore.BlobInfo.get(resource)
+		self.send_blob(blob_info, save_as=blob_info.filename)
+
 application = webapp2.WSGIApplication([('/', MainHandler),
 	('/upload', UploadHandler),
-	('/done', DoneHandler)],
-	debug=True)
+	('/done', DoneHandler),
+	('/download', DownloadHandler),
+	('/covert_room', DownloadHandler),
+	('/serve/([^/]+)?', ServeHandler),
+	], debug=True)
