3ef74af8778554cf9f03a6016ea19ea84946d6a9
Taesung Jung <tsj.july@gmail.com>
2014-06-22 17:51:59 +0900

    implemented send email feature closed #4

--- firenze.py
+++ firenze.py
@@ -3,11 +3,15 @@
 '''
 import os
 import urllib
+import hashlib
+import uuid
 
 from google.appengine.ext import ndb
 
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
+
+from google.appengine.api import mail
 
 import webapp2
 import jinja2
@@ -19,8 +23,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class CovertBox(ndb.Model):
 	key = ndb.BlobKeyProperty()
-	email = ndb.StringProperty()
-	generated_key = ndb.StringProperty()
 	file_name = ndb.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
@@ -35,12 +37,34 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
 	def post(self):
 		upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
 		user_key = self.request.get('user_key')
+		email = self.request.get('email')
+		
+		retrieval_key = None
 
-		box_instance = CovertBox(parent=ndb.Key('user_key', user_key))
+		# email address validation
+		if '' != email:
+			retrieval_key = hashlib.sha512(str(uuid.uuid4().get_hex()) + email).hexdigest()
+
+			message = mail.EmailMessage(
+					sender="? Covert-Box ?<covert-box@appspot.gserviceaccount.com>",
+					subject='"{}" has been uploaded'.format(upload_files[0].filename))
+
+			message.to = "<" + email + ">"
+			message.body = """Dear You,
+retrieval key: {}
+? Covert-Box ?
+""".format(retrieval_key)
+
+			message.send()
+
+		elif '' != user_key: # user_key validation
+			retrieval_key = user_key
+
+		box_instance = CovertBox(parent=ndb.Key('retrieval_key', str(retrieval_key)))
+
 		box_instance.key = upload_files[0].key()
-		box_instance.email = 'n'
-		box_instance.generated_key = 'n'
 		box_instance.file_name = upload_files[0].filename
+
 		box_instance.put()
 
 		self.redirect('/done')
@@ -48,6 +72,8 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
 class DoneHandler(webapp2.RequestHandler):
 	def get(self):
 		done_page = JINJA_ENVIRONMENT.get_template('done.html')
+
+
 		self.response.out.write(done_page.render({}))
 
 class DownloadHandler(webapp2.RequestHandler):
@@ -60,7 +86,7 @@ class DownloadHandler(webapp2.RequestHandler):
 
 		user_key = self.request.get('user_key')
 
-		box_query = CovertBox.query(ancestor=ndb.Key('user_key', user_key))
+		box_query = CovertBox.query(ancestor=ndb.Key('retrieval_key', user_key))
 		box_list = box_query.fetch()
 
 		page_value = {
