60759f42ecefe88016f0875d0a3d3ff2f61091c9
Taesung Jung <tsj.july@gmail.com>
2014-08-15 02:25:02 +0900

    fixed box_instance.file_name -> file_name

--- firenze.py
+++ firenze.py
@@ -85,7 +85,7 @@ your message: {}
 
 			file_name = f.filename
 			
-			encoded_str = email.header.decode_header(box_instance.file_name)
+			encoded_str = email.header.decode_header(file_name)
 
 			if not encoded_str[0][1]:
 				file_name = encoded_str[0][0].decode(encoded_str[0][1])
